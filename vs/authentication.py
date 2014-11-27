9dbf7073ee74963572e383d822f8fa58a4d85503
linweitong <linweitong@126.com>
2014-11-27 12:50:28 +0800

    m

--- vs/authentication.py
+++ vs/authentication.py
@@ -1,9 +1,11 @@
 from rest_framework import authentication
-from rest_framework import exceptions
 from models import VSUser
+from rest_framework import exceptions, HTTP_HEADER_ENCODING
+
 
 class TokenAuthentication(authentication.TokenAuthentication):
-   def authenticate_credentials(self, key):
+
+    def authenticate_credentials(self, key):
         try:
             token = self.model.objects.get(key=key)
         except self.model.DoesNotExist:
@@ -15,3 +17,30 @@ class TokenAuthentication(authentication.TokenAuthentication):
         token.user = VSUser.objects.get(id=token.user.id)
 
         return (token.user, token)
+
+    def get_authorization_header2(self, request):
+        """
+        Return request's 'VSAuthorization:' header, as a bytestring.
+
+        Hide some test client ickyness where the header can be unicode.
+        """
+        auth = request.META.get('HTTP_VSAUTHORIZATION', b'')
+        if isinstance(auth, type('')):
+            # Work around django test client oddness
+            auth = auth.encode(HTTP_HEADER_ENCODING)
+        return auth
+
+    def authenticate(self, request):
+        auth = self.get_authorization_header2(request).split()
+
+        if not auth or auth[0].lower() != b'token':
+            return None
+
+        if len(auth) == 1:
+            msg = 'Invalid token header. No credentials provided.'
+            raise exceptions.AuthenticationFailed(msg)
+        elif len(auth) > 2:
+            msg = 'Invalid token header. Token string should not contain spaces.'
+            raise exceptions.AuthenticationFailed(msg)
+
+        return self.authenticate_credentials(auth[1])
